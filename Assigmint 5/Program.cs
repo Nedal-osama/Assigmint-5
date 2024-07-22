aae68f0bcee8185298268da86fa5d61ed9ffe147
Nedal <nedalosama3@gmail.com>
2024-07-22 23:15:41 +0300

    second project

--- Assigmint 5/Program.cs
+++ Assigmint 5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Assigmint_5.First_project;
+using Assigmint_5.Second_project;
 
 namespace Assigmint_5
 {
@@ -7,36 +8,46 @@ namespace Assigmint_5
         static void Main(string[] args)
         {
             #region First project
-         /*   Point3D p1, p2;
-            try
-            {
-                Console.Write("Enter x, y, z for p1: ");
-                string[] coords = Console.ReadLine().Split(' ');
-                p1 = new Point3D(int.Parse(coords[0]), int.Parse(coords[1]), int.Parse(coords[2]));
-                Console.Write("Enter x, y, z for p2: ");
-                coords = Console.ReadLine().Split(' ');
-                p2 = new Point3D(int.Parse(coords[0]), int.Parse(coords[1]), int.Parse(coords[2]));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Invalid input: " + ex.Message);
-                return;
-            }
+            /*   Point3D p1, p2;
+               try
+               {
+                   Console.Write("Enter x, y, z for p1: ");
+                   string[] coords = Console.ReadLine().Split(' ');
+                   p1 = new Point3D(int.Parse(coords[0]), int.Parse(coords[1]), int.Parse(coords[2]));
+                   Console.Write("Enter x, y, z for p2: ");
+                   coords = Console.ReadLine().Split(' ');
+                   p2 = new Point3D(int.Parse(coords[0]), int.Parse(coords[1]), int.Parse(coords[2]));
+               }
+               catch (Exception ex)
+               {
+                   Console.WriteLine("Invalid input: " + ex.Message);
+                   return;
+               }
 
-            Console.WriteLine(p1.ToString());
-            Console.WriteLine(p2.ToString());
+               Console.WriteLine(p1.ToString());
+               Console.WriteLine(p2.ToString());
 
-            if (p1 == p2)
-                Console.WriteLine("p1 and p2 are equal");
-            else
-                Console.WriteLine("p1 and p2 are not equal");
+               if (p1 == p2)
+                   Console.WriteLine("p1 and p2 are equal");
+               else
+                   Console.WriteLine("p1 and p2 are not equal");
 
-            Point3D[] points = new Point3D[] { p1, p2, new Point3D(5, 5, 5), new Point3D(1, 1, 1) };
-            Array.Sort(points);
+               Point3D[] points = new Point3D[] { p1, p2, new Point3D(5, 5, 5), new Point3D(1, 1, 1) };
+               Array.Sort(points);
 
-            foreach (Point3D p in points)
-                Console.WriteLine(p.ToString());*/
+               foreach (Point3D p in points)
+                   Console.WriteLine(p.ToString());*/
             #endregion
+            #region SEcond project
+           /* int x = 10, y = 5;
+
+            Console.WriteLine("Add: " + Maths.Add(x, y));
+            Console.WriteLine("Subtract: " + Maths.Subtract(x, y));
+            Console.WriteLine("Multiply: " + Maths.Multiply(x, y));
+            Console.WriteLine("Divide: " + Maths.Divide(x, y));*/
+
+            #endregion
+
 
         }
     }
